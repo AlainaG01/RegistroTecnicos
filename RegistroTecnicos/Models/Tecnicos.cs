d7a1bb7ba08192e48a079f752e7ffc6f28f8d7d8
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-08-27 15:09:03 -0400

    Realicé el modelo Tecnicos

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Tecnicos
+{
+    [Key]
+    public int TecnicoId { get; set; }
+    public string? Nombre { get; set; }
+    public double? SueldoHora { get; set; }
+}
