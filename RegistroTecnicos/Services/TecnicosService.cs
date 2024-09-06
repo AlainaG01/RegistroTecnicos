b99bb3aa671cec00afe0cc50c1b8524acdee592b
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-09-06 00:17:28 -0400

    Realicé una correccion en el metodo buscar

--- RegistroTecnicos/Services/TecnicosService.cs
+++ RegistroTecnicos/Services/TecnicosService.cs
@@ -49,7 +49,7 @@ public class TecnicosService
     public async Task<Tecnicos> Buscar(int id)
     {
         return await _contexto.Tecnicos
-            .AsNoTracking().FirstOrDefaultAsync(e => e.TecnicoId == id);
+            .FirstOrDefaultAsync(e => e.TecnicoId == id);
     }
 
     public async Task<List<Tecnicos>> Listar(Expression<Func<Tecnicos, bool>> criterio)
