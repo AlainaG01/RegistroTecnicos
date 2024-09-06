b45dfd93bead230a00e49fcd1f9639d1e0e589d1
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-09-05 22:48:33 -0400

    Realicé el modelo TiposTecnicos

---
+++ RegistroTecnicos/Models/TiposTecnicos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class TiposTecnicos
+{
+    [Key]
+    public int TiposTecnicosId { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string? Descripcion { get; set; }
+}
