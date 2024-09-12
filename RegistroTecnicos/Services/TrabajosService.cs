13756168651607bae2b1b480a791241e01a390b5
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-09-12 00:30:02 -0400

    Realicé el service del modelo Trabajos

---
+++ RegistroTecnicos/Services/TrabajosService.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicos.Services;
+
+public class TrabajosService
+{
+    private readonly Contexto _contexto;
+
+    public TrabajosService(Contexto contexto)
+    {
+        _contexto = contexto;
+    }
+
+    private async Task<bool> Existe(int trabajoId)
+    {
+        return await _contexto.Trabajos.AnyAsync(e => e.TrabajoId == trabajoId);
+    }
+
+    private async Task<bool> Insertar(Trabajos trabajo)
+    {
+        _contexto.Trabajos.Add(trabajo);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    private async Task<bool> Modificar(Trabajos trabajo)
+    {
+        _contexto.Update(trabajo);
+        var modificado = await _contexto.SaveChangesAsync() > 0;
+        return modificado;
+    }
+
+    public async Task<bool> Guardar(Trabajos trabajo)
+    {
+        if(! await Existe(trabajo.TrabajoId))
+            return await Insertar(trabajo);
+        else
+            return await Modificar(trabajo);
+    }
+
+    public async Task<bool> Eliminar(int trabajoId)
+    {
+        return await _contexto.Trabajos
+            .Where(e => e.TrabajoId == trabajoId)
+            .ExecuteDeleteAsync() > 0;
+    }
+
+    public async Task<Trabajos> Buscar(int id)
+    {
+        return await _contexto.Trabajos
+            .Include(e => e.Tecnico).Include(e => e.Cliente)
+            .AsNoTracking()
+           .FirstOrDefaultAsync(e => e.TrabajoId == id);
+    }
+
+    public async Task<List<Trabajos>> Listar(Expression<Func<Trabajos, bool>> criterio)
+    {
+        return await _contexto.Trabajos.Include(e => e.Tecnico).Include(e => e.Cliente).AsNoTracking().Where(criterio).ToListAsync();
+    }
+
+    public async Task<bool> ExisteTrabajo(int trabajoId, string descripcion)
+    {
+        return await _contexto.Trabajos
+            .AnyAsync(e => e.TrabajoId !=  trabajoId 
+            && e.Descripcion.ToLower().Equals(descripcion.ToLower()));
+    }
+}
