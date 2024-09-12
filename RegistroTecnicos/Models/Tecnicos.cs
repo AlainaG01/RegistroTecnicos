f576a7cef76fef11649744c7510a317d909cc105
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-09-11 23:52:15 -0400

    Arregle la relacion entre Tecnico y TiposTecnicos utilizando llave foranea

--- RegistroTecnicos/Models/Tecnicos.cs
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RegistroTecnicos.Models;
 
@@ -7,14 +8,16 @@ public class Tecnicos
     [Key]
     public int TecnicoId { get; set; }
 
-    [Required(ErrorMessage = "Campo obligatorio")]
+    [Required(ErrorMessage = "Campo obligatorio N")]
     [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
     public string? Nombres { get; set; }
 
     [Required(ErrorMessage = "Campo obligatorio")]
-    public string? Descripcion { get; set; }
-
-    [Required(ErrorMessage = "Campo obligatorio")]
     [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten numeros enteros o decimales")]
     public double SueldoHora { get; set; }
+
+    [ForeignKey("TiposTecnicos")]
+    public int TiposTecnicosId { get; set; }
+
+    public TiposTecnicos? TipoTecnico { get; set; }
 }
