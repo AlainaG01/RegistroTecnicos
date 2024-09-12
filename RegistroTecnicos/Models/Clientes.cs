89e16f47806154c48ed93b605a1ccdeb2fce1bc6
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-09-12 00:22:28 -0400

    Realicé el modelo Clientes

---
+++ RegistroTecnicos/Models/Clientes.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Clientes
+{
+    [Key]
+    public int ClienteId { get; set; }
+
+    [Required(ErrorMessage ="Campo obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string? Nombres { get; set; }
+
+    [Required(ErrorMessage ="Campo obligatorio")]
+    [RegularExpression(@"^\d+$", ErrorMessage ="Solo se permiten numeros enteros")]
+    public string? WhatsApp { get; set; }
+}
