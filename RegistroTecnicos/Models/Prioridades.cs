cb48f5f3a284c506e75991063bec777268d23352
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-09-19 17:32:36 -0400

    Realicé el modelo Prioridades

---
+++ RegistroTecnicos/Models/Prioridades.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Prioridades
+{
+    [Key]
+    public int PrioridadesId {  get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo letras")]
+    public string? Descripcion { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    [RegularExpression(@"^[1-9]\d*$", ErrorMessage = "Solo números naturales")]
+    public int Tiempo { get; set; }
+}
