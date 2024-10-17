76459c84e3d6cfb56c23adc46307daa972c68724
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-10-17 09:27:41 -0400

    Modelo Articulos realizado

---
+++ RegistroTecnicos/Models/Articulos.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Articulos
+{
+    [Key]
+    public int ArticuloId { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se permiten numeros enteros o decimales")]
+    public string? Descripcion { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public double Costo { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public double Precio { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public int Existencia { get; set; }
+}
