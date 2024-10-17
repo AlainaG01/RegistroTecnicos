f1c1d29824295eccdf06de32850720df8d31afd2
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-10-17 09:29:55 -0400

    Service se TrabajosDetalle realizado

---
+++ RegistroTecnicos/Services/TrabajosDetalleService.cs
@@ -0,0 +1,17 @@
+﻿using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicos.Services;
+
+public class TrabajosDetalleService
+{
+    private readonly Contexto _contexto;
+
+    public TrabajosDetalleService(Contexto contexto)
+    {
+        _contexto = contexto;
+    }
+
+    
+}
