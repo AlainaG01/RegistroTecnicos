d94ad7ff8ce48658e08d970bcb88319836780d88
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-10-17 09:28:21 -0400

    Modelo TrabajosDetalle realizado

---
+++ RegistroTecnicos/Models/TrabajosDetalle.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class TrabajosDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    [ForeignKey("TrabajoId")]
+    public int TrabajoId { get; set; }
+    public Trabajos? Trabajo { get; set; }
+
+    [ForeignKey("ArticuloId")]
+    public int ArticuloId { get; set; }
+    public Articulos? Articulo { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public int Cantidad { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public double Precio { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public double Costo { get; set; }
+}
