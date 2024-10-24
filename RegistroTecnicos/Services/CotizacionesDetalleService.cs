23e2f6c51392aa784f66edfa309df2a622917aab
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-10-24 15:14:08 -0400

    Service de CotizacionesDetalle realizado

---
+++ RegistroTecnicos/Services/CotizacionesDetalleService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicos.Services;
+
+public class CotizacionesDetalleService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<List<Articulos>> Listar(Expression<Func<Articulos, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Articulos.Where(criterio).ToListAsync();
+    }
+}
