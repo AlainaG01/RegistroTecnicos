978c19e3574a36b67d05eb66322ff541dc26a45b
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-10-24 15:09:26 -0400

    Modelo CotizacionesDetalle realizado

---
+++ RegistroTecnicos/Models/CotizacionesDetalle.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class CotizacionesDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    [ForeignKey("Cotizaciones")]
+    public int CotizacionId { get; set; }
+    public Cotizaciones? Cotizacion { get; set; }
+
+    [ForeignKey("Articulos")]
+    public int ArticuloId { get; set; }
+    public Articulos? Articulo { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public int Cantidad { get; set; }
+
+    [Required(ErrorMessage = "Campo obligatorio")]
+    public double Precio {  get; set; }
+}
